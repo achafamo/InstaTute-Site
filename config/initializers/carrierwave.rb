45aa23e4f2e07b7f42683e49ca7bb5db03b69d77
achafamo <achafamo@colgate.edu>
2017-07-25 14:09:46 -0400

    carierwave added

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,27 @@
-CarrierWave.configure do |config|
-  if Rails.env.production?
+# config/initializers/carrierwave.rb
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+if Rails.env.test? or Rails.env.development?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.root = "#{Rails.root}/tmp"
+    config.cache_dir = "#{Rails.root}/tmp/images"
+    config.fog_directory = 'socify'
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :fog     
+	config.fog_provider = 'fog/aws'     
     config.fog_credentials = {
-      provider:              'AWS', # required
-      aws_access_key_id:     Rails.application.secrets.aws_access_key_id, # required
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-      region:                'us-east-1', # optional, defaults to 'us-east-1'
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region => ENV['S3_REGION']
     }
-    config.fog_directory  = 'socify' # required
-    config.fog_public     = true # optional, defaults to true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+    
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.asset_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
   end
 end
