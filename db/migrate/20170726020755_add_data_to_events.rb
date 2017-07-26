46c2789a129da5f234d129ff69a01fa9a8c1e500
achafamo <achafamo@colgate.edu>
2017-07-26 00:20:29 -0400

    migration

--- db/migrate/20170726020755_add_data_to_events.rb
+++
@@ -1,8 +0,0 @@
-class RenameEventsWhenColumn < ActiveRecord::Migration[5.0]
-  def self.down
-    remove_column :events, :start
-  end
-  def change
-    rename_column :events, :event_datetime, :start
-  end
-end
