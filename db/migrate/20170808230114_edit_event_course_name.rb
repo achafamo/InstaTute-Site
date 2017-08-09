6bef53d646a14b7d05bf479d4beb8675552e8417
achafamo <achafamo@colgate.edu>
2017-08-08 22:58:17 -0400

    stry

--- db/migrate/20170808230114_edit_event_course_name.rb
+++ db/migrate/20170808230114_edit_event_course_name.rb
@@ -1,4 +1,7 @@
 class EditEventCourseName < ActiveRecord::Migration[5.0]
+  def self.up
+		add_column :events, :course_name
+	end
   def change
 		change_column :events, :course_name, :string
   end
