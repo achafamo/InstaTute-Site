aea0b7dc2b31af5ad299bdecedf872dfacef1623
achafamo <achafamo@colgate.edu>
2017-08-08 23:02:02 -0400

    removed migration

--- db/migrate/20170808230114_edit_event_course_name.rb
+++
@@ -1,8 +0,0 @@
-class EditEventCourseName < ActiveRecord::Migration[5.0]
-  def self.up
-		add_column :events, :course_name
-	end
-  def change
-		change_column :events, :course_name, :string
-  end
-end
